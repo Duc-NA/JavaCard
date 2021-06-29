47df5890a51b65e342d237521510d88ab0d2f035
satthuno999 <44010887+duckmak14@users.noreply.github.com>
2021-06-30 02:59:36 +0700

    update

--- JavaCardCore/Project/src/AuthenticationPin/Infomation.java
+++ JavaCardCore/Project/src/AuthenticationPin/Infomation.java
@@ -1,21 +1,9 @@
 package AuthenticationPin;
 
 import javacard.framework.*;
-import org.globalplatform.GPSystem;
-import javacard.security.AESKey;
-import javacardx.crypto.Cipher;
-import javacard.security.KeyBuilder;
-import javacard.security.RandomData;
-
-
 
 public class Infomation extends Applet
 {
-	// khai bao bien
-	private Cipher aesCipher;
-    private AESKey aesKey;
-    private final static short KEY_SIZE = 32;
-    
 	//CLA
 	final static byte Bot_CLA = (byte) 0xB0;
 	
@@ -27,42 +15,18 @@ public class Infomation extends Applet
 	final static byte INS_COUNTINSERT = (byte)0x04;
 	final static byte INS_SETCOUNT = (byte)0x05;
 	final static byte INS_COUNTANH = (byte)0x06;
-	final static byte INS_BLOCK = (byte)0x07;
-	final static byte INS_UNBLOCK = (byte)0x08;
-	final static byte INS_ERROR = (byte)0x09;
-	final static byte INS_CHECKBLOCKE = (byte)0xA0;
-	final static byte INS_CHECKERROR = (byte)0x0A1;
 	
 	// variable
 	private static byte[] arrayhoten, arrayngaysinh, arrayCMND,arrayGPLX, arrayvehicle, image, size;
-	private static byte[] arrayhotenencrypt, arrayngaysinhencrypt, arrayCMNDencrypt,arrayGPLXencrypt, arrayvehicleencrypt;
-	// bien dem so lan vi pham giao thong
-	private short error = 0;
-	// neu nhu the block thi bien block = 1; Neu the khong bi block thi block = 0
-	private short block = 0;
 	final static byte phi = (byte) 0x03;
 	byte balance = (byte) 0x0A;
 	short countht, countns, countcmnd, countgplx, countvehicle;
-	private Infomation(byte[] bArray, short bOffset, byte bLength){
-		image = new byte[10000];
-		size = new byte[7];
-		aesCipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
-        aesKey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
-        byte[] keyBytes = JCSystem.makeTransientByteArray(KEY_SIZE, JCSystem.CLEAR_ON_DESELECT);
-        try {
-            RandomData rng = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
-            rng.generateData(keyBytes, (short) 0, KEY_SIZE);
-            aesKey.setKey(keyBytes, (short) 0);
-        } finally {
-            Util.arrayFillNonAtomic(keyBytes, (short) 0, KEY_SIZE, (byte) 0);
-        }
-        
-        register();
-	}
-	
+
 	public static void install(byte[] bArray, short bOffset, byte bLength) 
 	{
-		Infomation reg = new Infomation(bArray,bOffset,bLength);
+		new Infomation().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
+		image = new byte[10000];
+		size = new byte[7];
 	}
 
 	public void process(APDU apdu)
@@ -76,20 +40,14 @@ public class Infomation extends Applet
 		apdu.setIncomingAndReceive();
 		switch (buf[ISO7816.OFFSET_INS])
 		{
-			// case dem do dài cua thông tin truyen vào
-			// thông tin truyen vào se duoc phan cach boi dau !						
-			// all case thong tin Du lieu truyen vao phai giong nhau
-			// send = hoten+"!"+ngaysinh+"!"+diachi+"!"+bienks;
-			
 	case INS_COUNTINSERT:
 			short dataLen1 = (short)(buf[ISO7816.OFFSET_LC]&0xff);
 			short flag1 = (short)1;
-			// khai báo bien dem lenght
-			countht = 0; // h tên
-			countns = 0; // ngày sinh
-			countcmnd = 0; // cmnd
-			countgplx = 0; // gplx
-			countvehicle = 0; // phng tin
+			countht = 0;
+			countns = 0;
+			countcmnd = 0;
+			countgplx = 0;
+			countvehicle = 0;
 				for(short i = (short)(ISO7816.OFFSET_CDATA);i<(short)(ISO7816.OFFSET_CDATA +1+dataLen1);i++ ){
 					if(buf[i]==(byte)0x21){
 						flag1+=(short)1;
@@ -116,20 +74,18 @@ public class Infomation extends Applet
 			countgplx++;
 			countvehicle++;
 			break;
-			// luu thong tin vao array cua tung thong tin 
 		case INS_INSERT:
 			short dataLen = (short)(buf[ISO7816.OFFSET_LC]&0xff);
 			short flag = (short)1;
-			arrayhoten = new byte[countht]; // array ho tên
-			arrayngaysinh = new byte[countns]; // array ngay sinh
-			arrayCMND = new byte[countcmnd]; // array CMND
-			arrayGPLX = new byte[countgplx]; // array GPLX
-			arrayvehicle = new byte[countvehicle]; // array phung tien
-			
-			// vong lap luu thong tin
+			arrayhoten = new byte[countht];
+			arrayngaysinh = new byte[countns];
+			arrayCMND = new byte[countcmnd];
+			arrayGPLX = new byte[countgplx];
+			arrayvehicle = new byte[countvehicle];
+			//  copy length array 
 			short tempIndex = (short)0;
 				for(short i = (short)(ISO7816.OFFSET_CDATA);i<(short)(ISO7816.OFFSET_CDATA +1+dataLen);i++ ){
-					if(buf[i]==(byte)0x21){			
+					if(buf[i]==(byte)0x21){
 						if(flag ==(short)1){
 							arrayhoten[tempIndex++]=buf[i];
 						}
@@ -142,7 +98,7 @@ public class Infomation extends Applet
 							arrayGPLX[tempIndex++]=buf[i];
 						}else if(flag ==(short)5){
 							arrayvehicle[tempIndex++]=buf[i];
-						}			
+						}
 						flag+=(short)1;
 						tempIndex = (short)0;
 						continue;
@@ -162,46 +118,26 @@ public class Infomation extends Applet
 						arrayvehicle[tempIndex++]=buf[i];
 					}
 				}
-				// encrypt
-				 byte[] ht = encrypt(arrayhoten);
-				 byte[] ns = encrypt(arrayngaysinh);
-				 byte[] cmnd = encrypt(arrayCMND);
-				 byte[] gplx = encrypt(arrayGPLX);
-				 byte[] vc = encrypt(arrayvehicle);
-				 
-				 Util.arrayCopy(ht,(short)0,arrayhotenencrypt,(short)0, (short)ht.length);
-				 Util.arrayCopy(ns,(short)0,arrayngaysinhencrypt,(short)0, (short)ns.length);
-				 Util.arrayCopy(cmnd,(short)0,arrayCMNDencrypt,(short)0, (short)cmnd.length);
-				 Util.arrayCopy(gplx,(short)0,arrayGPLXencrypt,(short)0, (short)gplx.length);
-				 Util.arrayCopy(vc,(short)0,arrayvehicleencrypt,(short)0, (short)vc.length);
-			break;				
-						
-		case INS_THONGTIN:
-			// decrypt
-			byte[] htin = decrypt(arrayhotenencrypt);
-				 byte[] nsin = decrypt(arrayngaysinhencrypt);
-				 byte[] cmndin = decrypt(arrayCMNDencrypt);
-				 byte[] gplxin = decrypt(arrayGPLXencrypt);
-				 byte[] vcin = decrypt(arrayvehicleencrypt);
-				 
-			short lenhoten = (short) htin.length;
-			short lenngaysinh = (short) nsin.length;
-			short lencmnd = (short) cmndin.length;
-			short lengplx = (short) gplxin.length;
-			short lenvehicle = (short) vcin.length;
-			short len = (short) (lenhoten + lenngaysinh + lencmnd + lengplx + lenvehicle);
+			break;	
 			
+		case INS_THONGTIN:			
+			short lenhoten = (short) arrayhoten.length;
+			short lenngaysinh = (short) arrayngaysinh.length;
+			short lencmnd = (short) arrayCMND.length;
+			short lengplx = (short) arrayGPLX.length;
+			short lenvehicle = (short) arrayvehicle.length;
+			short len = (short) (lenhoten + lenngaysinh + lencmnd + lengplx + lenvehicle);
 			apdu.setOutgoing();
 			apdu.setOutgoingLength(len);
-			Util.arrayCopy(htin,(short)0,buf,(short)0,lenhoten);
+			Util.arrayCopy(arrayhoten,(short)0,buf,(short)0,lenhoten);
 			apdu.sendBytes((short)0, lenhoten);
-			Util.arrayCopy(nsin,(short)0,buf,(short)0,lenngaysinh);
+			Util.arrayCopy(arrayngaysinh,(short)0,buf,(short)0,lenngaysinh);
 			apdu.sendBytes((short)0, lenngaysinh);
-			Util.arrayCopy(cmndin,(short)0,buf,(short)0,lencmnd);
+			Util.arrayCopy(arrayCMND,(short)0,buf,(short)0,lencmnd);
 			apdu.sendBytes((short)0, lencmnd);
-			Util.arrayCopy(gplxin,(short)0,buf,(short)0,lengplx);
+			Util.arrayCopy(arrayGPLX,(short)0,buf,(short)0,lengplx);
 			apdu.sendBytes((short)0, lengplx);
-			Util.arrayCopy(vcin,(short)0,buf,(short)0,lenvehicle);
+			Util.arrayCopy(arrayvehicle,(short)0,buf,(short)0,lenvehicle);
 			apdu.sendBytes((short)0, lenvehicle);
 			break;		
 			
@@ -210,16 +146,13 @@ public class Infomation extends Applet
 			short count1 = (short)(249 * p1);
 			Util.arrayCopy(buf, ISO7816.OFFSET_CDATA, image, count1, (short)249);
 			break;
-			
 		case INS_SETCOUNT:
 			Util.arrayCopy(buf, ISO7816.OFFSET_CDATA, size, (short)0, (short)7);
 			break;
-			
 		case INS_COUNTANH:
 			Util.arrayCopy(size, (short)0, buf, (short)0, (short)(size.length));
 			apdu.setOutgoingAndSend((short)0,(short)7);
 			break;
-		
 		case INS_ANH:
 			apdu.setOutgoing();
 			short p = (short)(buf[ISO7816.OFFSET_P1]&0xff);
@@ -227,76 +160,9 @@ public class Infomation extends Applet
 			apdu.setOutgoingLength((short)249);
 			apdu.sendBytesLong(image, count, (short)249);
 			break;
-			// dem loi vi pham giao thong
-		case INS_ERROR:
-			error++;
-			break;
-			// check xem da vi pham bao nhieu loi
-		case INS_CHECKERROR:
-			byte[] checkerror = new byte[1];
-			checkerror[0] = (byte)error;
-			apdu.setOutgoing();
-			apdu.setOutgoingLength((short)1);
-			Util.arrayCopy(checkerror,(short)0,buf,(short)0,(short)1);
-			apdu.sendBytes((short)0, (short)1);
-			break;
-			// check xem co block hay khong
-		case INS_CHECKBLOCKE:
-			byte[] checkblock = new byte[1];
-			checkblock[0] = (byte)block;
-			apdu.setOutgoing();			
-			apdu.setOutgoingLength((short)1);
-			Util.arrayCopy(checkblock,(short)0,buf,(short)0,(short)1);
-			apdu.sendBytes((short)0, (short)1);
-			break;
-			// chuc nang block the
-		case INS_BLOCK:			
-			block = (short)1;
-			break;
-			// chuc nang unblock the
-		case INS_UNBLOCK:			
-			block = (short)0;
-			error = (short)0;
-			break;
 		default:
 			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
 		}
 	}
-/*AES*/
-	private byte[] encrypt(byte[] encryptData) {
-        aesCipher.init(aesKey, Cipher.MODE_ENCRYPT);
-        short newLength = addPadding(encryptData, (short) 0, (short) encryptData.length);
-        byte[] temp = JCSystem.makeTransientByteArray(newLength, JCSystem.CLEAR_ON_DESELECT);
-        aesCipher.doFinal(encryptData, (short) 0 , newLength, temp, (short) 0x00);
-        return temp;
-    }
-
-    
-    private byte[] decrypt(byte[] encryptData) {
-        aesCipher.init(aesKey, Cipher.MODE_DECRYPT);
-        byte[] temp = JCSystem.makeTransientByteArray((short) encryptData.length, JCSystem.CLEAR_ON_DESELECT);
-        aesCipher.doFinal(encryptData, (short) 0, (short) encryptData.length, temp, (short) 0x00);
-        short newLength = removePadding(temp, (short) encryptData.length);
-        return temp;
-    }
-   
-    private short addPadding(byte[] data, short offset, short length) {
-        data[(short) (offset + length++)] = (byte)0x80;
-        while (length < 16 || (length % 16 != 0)) {
-            data[(short) (offset + length++)] = 0x00;
-        }
-        return length;
-    }
-   
-    private short removePadding(byte[] buffer, short length) {
-        while ((length != 0) && buffer[(short) (length - 1)] == (byte) 0x00) {
-            length--;
-        }
-        if (buffer[(short) (length - 1)] != (byte) 0x80) {
-            ISOException.throwIt(ISO7816.SW_DATA_INVALID);
-        }
-        length--;
-        return length;
-    }
 
 }
--- README.md
+++ README.md
@@ -16,3 +16,15 @@ lần gọi tới sẽ được cộng thêm 1 lỗi
 * Nếu vi phạm lỗi rượu bia thì gọi đến hàm block() để block luôn thẻ đó. Có 1 biến đếm block thẻ chỉ có 0 hoặc là 1. 
 * Trước khi đăng nhập sẽ gọi đến hàm checkBlock(). Xem thẻ bị block hay chưa. hàm trả về là 0 thì là chưa cho phép đăng nhập nếu 1 là đã bị block và thông báo thẻ đã bị block. 
 * Sau mỗi một lần cộng thêm lỗi ở hàm error() sẽ gọi đến hàm checkerror() để kiểm tra xem đã vi phạm 3 lỗi hay chưa nếu 3 lỗi rồi thì gọi đến hàm block() để block thẻ lại.
+
+## II. Kịch bản chức năng ghi thông tin và in thông tin ra bên ngoài
+
+1. Thông tin sẽ được gửi vào theo một dạng cố định và với thứ tự INS như sau
+* Đầu tiên phải gọi tới INS_COUNTINSERT để có thể đếm độ dài của từng trường một
+* Sau khi đếm được độ dài từng array rồi thì đến INS_INSERT để có thể lưu được thông tin vào card 
+* Muốn gọi thông tin ra bên ngoài thì gọi đến INS_THONGTIN
+* lưu ý khi sử dụng 2 INS đầu tiên thì dữ liệu gửi vào phải giống nhau và có cú pháp như sau
+
+```
+Họ tên!Ngày sinh!CMND!GPLX!Phương tiện!
+```
